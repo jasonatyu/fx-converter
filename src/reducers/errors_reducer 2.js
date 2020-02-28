724e691f2b44ce1bbd96f6e111c8be43f7ed7e11
Jason Yu <jason@jasonyu.co>
2020-02-27 20:27:57 -0800

    Remove duplicate reducers

--- src/reducers/errors_reducer 2.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import fxErrorsReducer from "./fx_errors_reducer";
-
-const errorsReducer = combineReducers({
-    fx: fxErrorsReducer
-});
-
-export default errorsReducer;
--- src/reducers/ui_reducer 2.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import filters from './filter_reducer';
-
-export default combineReducers({
-    filters
-});
